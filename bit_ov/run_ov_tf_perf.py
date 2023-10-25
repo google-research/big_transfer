771ab5b0a3de13c07e471ee89a9fbb98cd2bae38
Pradeep Sakhamoori <pradeep_sto@yahoo.com>
2023-10-25 14:12:24 -0700

    Update run_ov_tf_perf.py

--- bit_ov/run_ov_tf_perf.py
+++ bit_ov/run_ov_tf_perf.py
@@ -1,17 +1,4 @@
 """
- Copyright (c) 2022 Intel Corporation
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-      http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
-
-"""
 This script is tested with TensorFlow v2.9.1 and OpenVINO v2022.2.0
 Usage Example below. -tf, -ov are required, rest are optional:
 python run_ov_tf_perf.py \
